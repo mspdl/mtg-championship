6f67b8b2c7b9fdaa9ee1c68b7c18d704a9bab0aa
Morgan <morgan.espindola@gmail.com>
2024-10-20 17:13:36 -0300

    Added validation on record new round button

--- src/app/pages/rounds/new-round/new-round.component.ts
+++ src/app/pages/rounds/new-round/new-round.component.ts
@@ -1,17 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
 import { Router } from '@angular/router';
 import { Round } from '../../../interfaces/round.interface';
 import { RoundService } from '../../../services/round/round.service';
-import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-new-round',
   templateUrl: './new-round.component.html',
   styleUrls: ['./new-round.component.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule, FormsModule, ReactiveFormsModule, MatButton],
+  imports: [
+    CommonModule,
+    FormsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButton,
+  ],
 })
 export class NewRoundComponent implements OnInit {
   round: Round = <Round>{};
@@ -28,5 +34,11 @@ export class NewRoundComponent implements OnInit {
   saveRound() {
     this.roundService.registerRound(this.round);
     this.router.navigate(['/rounds']);
+  }
+
+  isRoundValid(): boolean {
+    return this.round.games.every(
+      (game) => game.score1 === 2 || game.score2 === 2
+    );
   }
 }
