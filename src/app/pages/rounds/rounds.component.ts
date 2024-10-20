02238271cd97d049495a60b4c73841f08278f3b3
Morgan <morgan.espindola@gmail.com>
2024-10-20 01:48:54 -0300

    Finished rounds styles

--- src/app/pages/rounds/rounds.component.ts
+++ src/app/pages/rounds/rounds.component.ts
@@ -1,23 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatButton } from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
 import { Round } from '../../interfaces/round.interface';
 import { RoundService } from '../../services/round/round.service';
-import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-rounds',
   templateUrl: './rounds.component.html',
   styleUrls: ['./rounds.component.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule, MatButton, MatExpansionModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatIconModule,
+  ],
 })
 export class RoundsComponent implements OnInit {
   rounds: Round[] = [];
   readonly panelOpenState = signal(false);
-
 
   constructor(
     private readonly roundService: RoundService,
