aa8dc25fa3640dde5ac2e679bf561da0b1b262ed
Morgan <morgan.espindola@gmail.com>
2024-10-20 22:31:58 -0300

    Improved logic

--- src/app/services/score/score.service.ts
+++ src/app/services/score/score.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Game } from '../../interfaces/game.interface';
+import { Player } from '../../interfaces/player.interface';
 import { PlayerService } from '../player/player.service';
 
 @Injectable({
@@ -8,36 +9,42 @@ import { PlayerService } from '../player/player.service';
 export class ScoreService {
   constructor(private readonly playerService: PlayerService) {}
 
-  updateScore(game: Game) {
+  updateScore(game: Game): void {
     const players = this.playerService.getPlayers();
 
     players.forEach((player) => {
-      if (player.id == game.player1.id) {
-        player.score += game.score1;
-
-        if (game.score1 == 2) {
-          player.winTimes += 1;
-          if (game.score2 == 0) {
-            player.win2x0Times += 1;
-          } else {
-            player.win2x1Times += 1;
-          }
-        }
-      }
-      if (player.id == game.player2.id) {
-        player.score += game.score2;
-
-        if (game.score2 == 2) {
-          player.winTimes += 1;
-          if (game.score1 == 0) {
-            player.win2x0Times += 1;
-          } else {
-            player.win2x1Times += 1;
-          }
-        }
+      if (this.isPlayerInGame(player, game.player1)) {
+        this.updatePlayerScore(player, game.score1, game.score2);
+      } else if (this.isPlayerInGame(player, game.player2)) {
+        this.updatePlayerScore(player, game.score2, game.score1);
       }
     });
 
     this.playerService.setPlayers(players);
+  }
+
+  private isPlayerInGame(player: Player, gamePlayer: Player): boolean {
+    return player.id === gamePlayer.id;
+  }
+
+  private updatePlayerScore(player: Player, playerScore: number, opponentScore: number): void {
+    player.score += playerScore;
+
+    if (this.isWinner(playerScore)) {
+      this.incrementWinCounters(player, opponentScore);
+    }
+  }
+
+  private isWinner(score: number): boolean {
+    return score === 2;
+  }
+
+  private incrementWinCounters(player: Player, opponentScore: number): void {
+    player.winTimes += 1;
+    if (opponentScore === 0) {
+      player.win2x0Times += 1;
+    } else {
+      player.win2x1Times += 1;
+    }
   }
 }
